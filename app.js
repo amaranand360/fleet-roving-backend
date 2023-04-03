b4d0c96124cef403b9311f94e85e781e30768ede
THARUN00001 <bpatharun@gmail.com>
2023-04-03 15:33:01 +0530

    gg

--- app.js
+++ app.js
@@ -302,7 +302,7 @@ console.log(req.isAuthenticated + "adminDAshboard");
 app.get('/logout', function(req, res, next){
   req.logout(function(err) {
     if (err) { return next(err); }
-    res.redirect('/login');
+    res.redirect('/login'); 
   });
 });
 
